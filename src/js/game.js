9b2192a7144baf5bcac58ed3ec47aecea7db7673
Jerome Lecomte <jerome@herebefrogs.com>
2016-08-30 21:10:11 -0400

    create and render our hero, a couple of glitchy androids and some innocent bystanders

--- src/js/game.js
+++ src/js/game.js
@@ -3,9 +3,37 @@ var canvas,
     currentTime,
     lastTime,
     elapsedTime,
-    HEIGHT = 600,
-    WIDTH = 600;
+    entities = [],
+    MAX_ANDROIDS = 5,
+    MIN_ANDROIDS = 1,
+    MAX_BYSTANDERS = 100,
+    MIN_BYSTANDERS = 25,
+    HEIGHT = 300,
+    WIDTH = 400;
 
+function randomInt(min, max) {
+  return Math.floor(Math.random() * (max + 1 - min) + min);
+
+}
+function createEntity(color) {
+  var entity = {
+    color: color,
+    height: 10,
+    width: 10,
+  };
+  entity.x = randomInt(entity.width, WIDTH - entity.width);
+  entity.y = randomInt(entity.height, HEIGHT - entity.height);
+  return entity;
+}
+
+function renderEntities(entities) {
+  entities.forEach(function(entity) {
+    ctx.fillStyle = entity.color;
+    ctx.fillRect(entity.x, HEIGHT - entity.y, entity.width, entity.height);
+});
+}
+
+// Game loop
 function init() {
   document.title = 'Blade Gunner';
 
@@ -13,6 +41,17 @@ function init() {
   canvas.width = WIDTH;
   canvas.height = HEIGHT;
   ctx = canvas.getContext('2d');
+
+  // hero
+  entities.push(createEntity('#00DD00'));
+  // glitchy androids
+  for (var n = randomInt(MIN_ANDROIDS, MAX_ANDROIDS); n > 0; n--) {
+    entities.push(createEntity('#DD0000'));
+  }
+  // bystanders
+  for (var n = randomInt(MIN_BYSTANDERS, MAX_BYSTANDERS); n > 0; n--) {
+    entities.push(createEntity('#0000DD'));
+  }
 
   lastTime = Date.now();
   loop();
@@ -27,13 +66,12 @@ function loop() {
 };
 
 function update(e) {
-  console.log('update ' + e);
 };
 
 function render() {
   ctx.clearRect(0, 0, WIDTH, HEIGHT);
 
-  console.log('render');
+  renderEntities(entities);
 };
 
 addEventListener('load', init);
